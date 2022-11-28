ebf19de6d4d38b5d6c7f4c5ed4bf75de63dd9c90
Josey Sekine <ipho.7johero@icloud.com>
2022-11-28 14:54:24 -0600

    done

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_202424) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
