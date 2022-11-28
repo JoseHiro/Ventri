29bae852a705e71038c3c7602d475abae6459ee1
Emilio Said <saidmacciseemilio@gmail.com>
2022-11-28 15:47:57 -0600

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :cars, only: [:new, :create]
+  resources :cars do
+    resources :rentals, only: [:new, :create]
+  end
+  resources :rentals, except: [:new, :create]
+
+  resources :rentals do
+    resources :reviews
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
