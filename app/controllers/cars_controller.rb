aa80c7068904d618f8a38c29969825f0887cee3d
Iskander Galbraith <ijgalbraith@protonmail.ch>
2022-11-30 15:21:37 -0600

    geocoding for car index working, started for show.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,13 @@
 class CarsController < ApplicationController
   def index
     @cars = Car.all
+    @markers = @cars.geocoded.map do |car|
+      {
+        lat: car.latitude,
+        lng: car.longitude,
+        info_window: render_to_string(partial: "car_info_window", locals: { car: car})
+      }
+    end
   end
 
   def new
@@ -20,6 +27,13 @@ class CarsController < ApplicationController
   def show
     @rental = Rental.new
     @car = Car.find(params[:id])
+    car = [@car]
+    @marker = car.geocoded.map do |vehicle|
+      {
+        lat: car.latitude,
+        lng: car.longitude
+      }
+    end
   end
 
   def edit
