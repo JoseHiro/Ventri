d82425cede0142f635dff53c7927686b72005a70
Iskander Galbraith <ijgalbraith@protonmail.ch>
2022-12-01 17:39:14 -0600

    began search implementation

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,17 +2,11 @@ class Car < ApplicationRecord
   belongs_to :owner, class_name: :User, foreign_key: :user_id
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  MODELS = []
 
-  def self.brands
-    getting_models
-  end
-
-  private
-
-  def self.getting_models
-    all.map do |car|
-      car.model
-    end
-  end
+  # include PgSearch::Model
+  # pg_search_scope :search_by_car,
+  #                 against: [ :brand, :model, :number_of_seats, :category, :color, :address],
+  #                 using: {
+  #                   tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #                 }
 end
