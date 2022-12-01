1d7618dac5eb1cf9df4402250b184edc7e2648eb
Iskander Galbraith <ijgalbraith@protonmail.ch>
2022-12-01 15:12:41 -0600

    style balances completed for index and show for cars - map vs cards scroll implemented and on brand styles implemented

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,17 @@ class Car < ApplicationRecord
   geocoded_by :address
   belongs_to :owner, class_name: :User, foreign_key: :user_id
   after_validation :geocode, if: :will_save_change_to_address?
+  MODELS = []
+
+  def self.brands
+    getting_models
+  end
+
+  private
+
+  def self.getting_models
+    all.map do |car|
+      car.model
+    end
+  end
 end
