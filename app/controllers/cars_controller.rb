b0c224970a35e437dbf57629584f7f2ff54df9b0
Iskander Galbraith <ijgalbraith@protonmail.ch>
2022-12-02 12:28:57 -0600

    styling added to the search bar

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,6 @@
 class CarsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     if params[:query].present?
       unless params[:address].empty?
